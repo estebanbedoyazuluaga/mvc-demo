ac34c04c3881669021c353e4548c1cab19bf9ca4
Esteban Bedoya <ebedoya83134@umanizales.edu.co>
2023-09-13 04:01:54 -0500

    stuff

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, url_for
 from routes.tasks_bp import bp
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return redirect(url_for("tasks.show_index"))
+
 app.register_blueprint(bp, url_prefix='/tasks')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7777, debug=True)
